cd71a9e2b49577bbed67c847e5d0ec4018d38858
X GILLES <xg353@free.fr>
2015-03-26 16:32:47 +0100

    Add IPV6 support for main-posix

--- main-posix.c
+++ main-posix.c
@@ -11,17 +11,31 @@
 int main(int argc, char **argv)
 {
     int fd;
+#ifdef IPV6
+    struct sockaddr_in6 servaddr, cliaddr;
+#else /* IPV6 */
     struct sockaddr_in servaddr, cliaddr;
+#endif /* IPV6 */
     uint8_t buf[4096];
     uint8_t scratch_raw[4096];
     coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 
+#ifdef IPV6
+    fd = socket(AF_INET6,SOCK_DGRAM,0);
+#else /* IPV6 */
     fd = socket(AF_INET,SOCK_DGRAM,0);
+#endif /* IPV6 */
 
     bzero(&servaddr,sizeof(servaddr));
+#ifdef IPV6
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_addr = in6addr_any;
+    servaddr.sin6_port = htons(PORT);
+#else /* IPV6 */
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(PORT);
+#endif /* IPV6 */
     bind(fd,(struct sockaddr *)&servaddr, sizeof(servaddr));
 
     endpoint_setup();
